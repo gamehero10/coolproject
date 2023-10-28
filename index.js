a1d2d4bcf5113c1ea8a169db8c210411f95adb34
Rafael Deluna <delunarafael@yahoo.com>
2023-10-28 04:42:51 -0400

    Added new files

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+
+server.get('/hello', (req, res) => {
+  res.json('hello, there');
+});
+
+server.use('*', (req, res) => {
+  res.json({ message: 'API is UP!' })
+});
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+
